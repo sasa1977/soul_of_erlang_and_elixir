4098f97351360145f36181054e1eac7573e1dfe6
Saša Jurić <sasa.juric@gmail.com>
2024-10-26 15:48:14 +0200

    Generate new phoenix project
    Generated with `mix phx.new . --app my_system --no-gettext --no-mailer --no-ecto`

---
+++ lib/my_system/application.ex
@@ -0,0 +1,33 @@
+defmodule MySystem.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      MySystemWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:my_system, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: MySystem.PubSub},
+      # Start a worker by calling: MySystem.Worker.start_link(arg)
+      # {MySystem.Worker, arg},
+      # Start to serve requests, typically the last entry
+      MySystemWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MySystem.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    MySystemWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
