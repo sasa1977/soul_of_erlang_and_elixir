4ae1bb3b311c393c91b6a5bc9b4d23de2fe25063
Saša Jurić <sasa.juric@gmail.com>
2024-10-27 19:13:58 +0100

    Add parent dep

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule MySystem.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:parent, "~> 0.12.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
